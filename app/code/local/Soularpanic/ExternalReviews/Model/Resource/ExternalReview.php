62f34770dbed2ff6c827ace2c65e1433825baaee
Joshua Kossoff <kossoff.j@gmail.com>
2014-10-07 18:19:10 -0400

    Adding external reviews.

--- app/code/local/Soularpanic/ExternalReviews/Model/Resource/ExternalReview.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Soularpanic_ExternalReviews_Model_Resource_ExternalReview
-    extends Mage_Core_Model_Resource_Db_Abstract {
-
-    protected function _construct() {
-        $this->_init('externalreviews/ExternalReview', 'entity_id');
-    }
-
-}
