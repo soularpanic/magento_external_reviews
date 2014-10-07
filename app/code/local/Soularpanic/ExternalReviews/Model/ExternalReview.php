74659df6b60a85a1052d75f72c82f35b0705d95d
Joshua Kossoff <kossoff.j@gmail.com>
2014-10-07 16:21:05 -0400

    Adding base model files

---
+++ app/code/local/Soularpanic/ExternalReviews/Model/ExternalReview.php
@@ -0,0 +1,9 @@
+<?php
+class Soularpanic_ExternalReviews_Model_ExternalReview
+extends Mage_Core_Model_Abstract {
+
+    protected function _construct() {
+        $this->_init('externalreviews/ExternalReview');
+    }
+
+}
---
+++ app/code/local/Soularpanic/ExternalReviews/Model/Resource/Setup.php
@@ -0,0 +1,4 @@
+<?php
+
+class Soularpanic_ExternalReviews_Model_Resource_Setup
+    extends Mage_Core_Model_Resource_Setup {}
